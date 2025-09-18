3e954010fa396ae924f20b321a1e9904436a6eb4
willbrid <ngaswilly77@gmail.com>
2025-09-18 05:40:04 +0200

    feat: implement bootstrap process with app package

---
+++ internal/app/app.go
@@ -0,0 +1,54 @@
+package app
+
+import (
+	"api-gateway-sql/config"
+	"api-gateway-sql/internal/delivery"
+	"api-gateway-sql/internal/repository"
+	"api-gateway-sql/internal/usecase"
+	"api-gateway-sql/pkg/httpserver"
+	"api-gateway-sql/pkg/logger"
+
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Run(cfgfile *config.Config, cfgflag *config.ConfigFlag) {
+	repos := repository.NewRepositories()
+	usecases := usecase.NewUsecases(usecase.Deps{
+		Repos: repos,
+	}, cfgfile)
+
+	handlers := delivery.NewHandler(usecases)
+	httpServer := httpserver.NewServer(
+		fmt.Sprint(":"+fmt.Sprint(cfgflag.ListenPort)),
+		cfgflag.EnableHttps,
+		cfgflag.CertFile,
+		cfgflag.KeyFile,
+	)
+	handlers.InitRouter(httpServer.Router, cfgfile)
+	httpServer.Start()
+
+	var logInfoServer string
+	if cfgflag.EnableHttps {
+		logInfoServer = "app server is listening on port %v using https"
+	} else {
+		logInfoServer = "app server is listening on port %v using http"
+	}
+
+	logger.LogInfo(logInfoServer, cfgflag.ListenPort)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		logger.LogInfo("app server - run - signal: %s", s.String())
+	case err := <-httpServer.Notify():
+		logger.LogError("app server error: %v", err.Error())
+	}
+
+	if err := httpServer.Stop(); err != nil {
+		logger.LogError("app server - stop - error: %v", err)
+	}
+}
