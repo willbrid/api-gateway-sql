bfefeeaf9d5c3abacba7e9acf375d365591567e6
willbrid <ngaswilly77@gmail.com>
2025-09-18 18:30:55 +0200

    feat: implement post and get sql handler

--- internal/delivery/httphandler/sqlqueryhandler.go
+++ internal/delivery/httphandler/sqlqueryhandler.go
@@ -1,7 +1,20 @@
 package httphandler
 
 import (
+	"api-gateway-sql/internal/domain"
+	"api-gateway-sql/pkg/httpresponse"
+	"api-gateway-sql/pkg/logger"
+	"encoding/json"
+
+	"context"
 	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	successAPIMessage string = "operation completed successfully"
+	failedAPIMessage  string = "Operation ended in failure"
 )
 
 // HandleHealthCheck godoc
@@ -30,7 +43,25 @@ func (h *HTTPHandler) HandleHealthCheck(resp http.ResponseWriter, req *http.Requ
 // @Security     BasicAuth
 // @Router       /api-gateway-sql/{target} [get]
 func (h *HTTPHandler) ApiGetSqlHandler(resp http.ResponseWriter, req *http.Request) {
+	var (
+		vars       map[string]string = mux.Vars(req)
+		targetName string            = vars["target"]
+		ctx        context.Context   = req.Context()
+	)
 
+	sqlqueryInput := &domain.SQLQueryInput{
+		TargetName: targetName,
+		PostParams: make(map[string]any, 0),
+	}
+
+	sqlqueryOutput, err := h.Usercases.ISQLQueryUsecase.ExecuteSingle(ctx, sqlqueryInput)
+	if err != nil {
+		logger.LogError("error: %s", err.Error())
+		httpresponse.SendJSONResponse(resp, http.StatusInternalServerError, failedAPIMessage, nil)
+		return
+	}
+
+	httpresponse.SendJSONResponse(resp, http.StatusOK, successAPIMessage, sqlqueryOutput)
 }
 
 // ApiPostSqlHandler godoc
@@ -47,5 +78,30 @@ func (h *HTTPHandler) ApiGetSqlHandler(resp http.ResponseWriter, req *http.Reque
 // @Security     BasicAuth
 // @Router       /api-gateway-sql/{target} [post]
 func (h *HTTPHandler) ApiPostSqlHandler(resp http.ResponseWriter, req *http.Request) {
+	var (
+		vars       map[string]string = mux.Vars(req)
+		targetName string            = vars["target"]
+		ctx        context.Context   = req.Context()
+	)
 
+	var postParams map[string]any
+	if err := json.NewDecoder(req.Body).Decode(&postParams); err != nil {
+		logger.LogError("error: %s", err.Error())
+		httpresponse.SendJSONResponse(resp, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+
+	sqlqueryInput := &domain.SQLQueryInput{
+		TargetName: targetName,
+		PostParams: postParams,
+	}
+
+	sqlqueryOutput, err := h.Usercases.ISQLQueryUsecase.ExecuteSingle(ctx, sqlqueryInput)
+	if err != nil {
+		logger.LogError("error: %s", err.Error())
+		httpresponse.SendJSONResponse(resp, http.StatusInternalServerError, failedAPIMessage, nil)
+		return
+	}
+
+	httpresponse.SendJSONResponse(resp, http.StatusOK, successAPIMessage, sqlqueryOutput)
 }
