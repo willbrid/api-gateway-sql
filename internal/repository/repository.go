1748f56d657d7e7ae7ecd90f996432db6df43bcf
willbrid <ngaswilly77@gmail.com>
2025-09-18 22:11:12 +0200

    feat: implement sql init database repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,12 +10,18 @@ type ISQLQueryRepo interface {
 	Execute(ctx context.Context, query string, params map[string]any) (*domain.SQLQueryOutput, error)
 }
 
+type ISQLInitDatabaseRepo interface {
+	ExecuteInit(ctx context.Context, sqlQueries []string) error
+}
+
 type Repositories struct {
-	ISQLQueryRepo ISQLQueryRepo
+	ISQLQueryRepo        ISQLQueryRepo
+	ISQLInitDatabaseRepo ISQLInitDatabaseRepo
 }
 
 func NewRepositories() *Repositories {
 	return &Repositories{
-		ISQLQueryRepo: NewSQLQueryRepo(),
+		ISQLQueryRepo:        NewSQLQueryRepo(),
+		ISQLInitDatabaseRepo: NewSQLInitDatabaseRepo(),
 	}
 }
---
+++ internal/repository/sqlinitdatabase.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"context"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+type SQLInitDatabaseRepo struct {
+	db *gorm.DB
+}
+
+func NewSQLInitDatabaseRepo() *SQLInitDatabaseRepo {
+	return &SQLInitDatabaseRepo{}
+}
+
+func (i *SQLInitDatabaseRepo) SetDB(db *gorm.DB) {
+	i.db = db
+}
+
+func (i *SQLInitDatabaseRepo) ExecuteInit(ctx context.Context, sqlQueries []string) error {
+	return i.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		for _, sqlQuery := range sqlQueries {
+			query := strings.TrimSpace(sqlQuery)
+			if query != "" {
+				if err := tx.Exec(query).Error; err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+}
