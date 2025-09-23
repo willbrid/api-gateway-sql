42e3bf7b280c83872f091767312431cf35b26782
willbrid <ngaswilly77@gmail.com>
2025-09-23 13:09:23 +0200

    refactor: update repo interfaces

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"api-gateway-sql/internal/domain"
+
+	"gorm.io/gorm"
 )
 
 type ISQLQueryRepo interface {
@@ -14,14 +16,32 @@ type ISQLInitDatabaseRepo interface {
 	ExecuteInit(ctx context.Context, sqlQueries []string) error
 }
 
+type IBatchStat interface {
+	Create(ctx context.Context, targetName string) (string, error)
+	UpdateLastCompleted(ctx context.Context, batchStat *domain.BatchStat) error
+	AddBlockToBatchStat(ctx context.Context, bs *domain.BatchStat, block *domain.Block) (*domain.Block, error)
+	FindAll(ctx context.Context, offset, limit int) ([]*domain.BatchStat, int64, error)
+	FindById(ctx context.Context, uid string) (*domain.BatchStat, error)
+}
+
+type IBlock interface {
+	Update(ctx context.Context, block *domain.Block, failureRange *domain.FailureRange, isSuccess bool) error
+	FindAllByBatchStatID(ctx context.Context, batchStatId string, offset, limit int) ([]*domain.Block, int64, error)
+	FindById(ctx context.Context, uid string) (*domain.Block, error)
+}
+
 type Repositories struct {
 	ISQLQueryRepo        ISQLQueryRepo
 	ISQLInitDatabaseRepo ISQLInitDatabaseRepo
+	IBatchStat           IBatchStat
+	IBlock               IBlock
 }
 
-func NewRepositories() *Repositories {
+func NewRepositories(appDb *gorm.DB) *Repositories {
 	return &Repositories{
 		ISQLQueryRepo:        NewSQLQueryRepo(),
 		ISQLInitDatabaseRepo: NewSQLInitDatabaseRepo(),
+		IBatchStat:           NewBatchStatRepo(appDb),
+		IBlock:               NewBlockRepo(appDb),
 	}
 }
