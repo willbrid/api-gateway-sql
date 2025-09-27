588d7ddced16eae808a3714859f8f04fad377639
willbrid <ngaswilly77@gmail.com>
2025-09-27 20:09:46 +0200

    refactor: rename package and enforce func to return channel

---
+++ pkg/csvstream/reader.go
@@ -0,0 +1,61 @@
+package csvstream
+
+import (
+	"encoding/csv"
+	"io"
+	"mime/multipart"
+	"strings"
+)
+
+type Block struct {
+	StartLine int
+	EndLine   int
+	Lines     [][]string
+}
+
+func ReadCSVInBlock(file multipart.File, blockSize int) (chan *Block, chan error) {
+	var (
+		blockChannel chan *Block
+		errorChannel chan error
+	)
+
+	go func() {
+		reader := csv.NewReader(file)
+		numLine := 0
+
+		defer close(blockChannel)
+		defer close(errorChannel)
+
+		for {
+			block := &Block{
+				StartLine: blockSize*numLine + 1,
+				Lines:     make([][]string, 0, blockSize),
+			}
+
+			for i := 0; i < blockSize; i++ {
+				record, err := reader.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					errorChannel <- err
+					return
+				}
+				block.Lines = append(block.Lines, strings.Split(record[0], ";"))
+			}
+
+			if len(block.Lines) == 0 {
+				break
+			}
+
+			block.EndLine = block.StartLine + len(block.Lines) - 1
+			blockChannel <- block
+			numLine++
+		}
+
+		close(blockChannel)
+		close(errorChannel)
+	}()
+
+	return blockChannel, errorChannel
+}
