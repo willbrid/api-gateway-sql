527f198eaca12142d27966651455b75052c03516
willbrid <ngaswilly77@gmail.com>
2025-09-27 20:12:43 +0200

    feat(refactor): define batch query usecase interface

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -12,6 +12,10 @@ type ISQLQueryUsecase interface {
 	ExecuteSingle(ctx context.Context, sqlquery *domain.SQLQueryInput) (*domain.SQLQueryOutput, error)
 }
 
+type ISQLBatchQueryUsecase interface {
+	ExecuteBatch(ctx context.Context, sqlbatchquery *domain.SQLBatchQueryInput) error
+}
+
 type ISQLInitDatabaseUsecase interface {
 	ExecuteInit(ctx context.Context, sqlinit *domain.SQLInitDatabaseInput) error
 }
@@ -19,6 +23,7 @@ type ISQLInitDatabaseUsecase interface {
 type Usecases struct {
 	ISQLQueryUsecase        ISQLQueryUsecase
 	ISQLInitDatabaseUsecase ISQLInitDatabaseUsecase
+	ISQLBatchQueryUsecase   ISQLBatchQueryUsecase
 }
 
 type Deps struct {
@@ -29,9 +34,16 @@ type Deps struct {
 func NewUsecases(deps Deps) *Usecases {
 	sqlQueryUsecase := NewSQLQueryUsecase(deps.Repos.ISQLQueryRepo.(*repository.SQLQueryRepo), deps.Config)
 	sqlInitDatabaseUsecase := NewSQLInitDatabaseUsecase(deps.Repos.ISQLInitDatabaseRepo.(*repository.SQLInitDatabaseRepo), deps.Config)
+	sqlBatchQueryUsecase := NewSQLBatchQueryUsecase(
+		deps.Repos.ISQLQueryRepo.(*repository.SQLQueryRepo),
+		deps.Repos.IBatchStat.(*repository.BatchStatRepo),
+		deps.Repos.IBlock.(*repository.BlockRepo),
+		deps.Config,
+	)
 
 	return &Usecases{
 		ISQLQueryUsecase:        sqlQueryUsecase,
 		ISQLInitDatabaseUsecase: sqlInitDatabaseUsecase,
+		ISQLBatchQueryUsecase:   sqlBatchQueryUsecase,
 	}
 }
