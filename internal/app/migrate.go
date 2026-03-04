647c5a4970588fbd6311cb3d2b8d3adb523bb670
willbrid <ngaswilly77@gmail.com>
2026-03-04 08:00:44 +0100

    refactor: return formated error

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -1,15 +1,18 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/willbrid/api-gateway-sql/internal/domain"
-	"github.com/willbrid/api-gateway-sql/pkg/logger"
 
 	"gorm.io/gorm"
 )
 
-func MigrateAppDatabase(db *gorm.DB, iLogger logger.ILogger) {
+func MigrateAppDatabase(db *gorm.DB) error {
 	err := db.AutoMigrate(&domain.BatchStat{}, &domain.Block{}, &domain.FailureRange{})
 	if err != nil {
-		iLogger.Error("error during migrations: %s", err.Error())
+		return fmt.Errorf("failed to execute migration: %w", err)
 	}
+
+	return nil
 }
